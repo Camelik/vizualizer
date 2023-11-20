309184c8a2d9b51b3f9eed2a6017909f0cea9139
Kamil <menadzer.odzidzi@gmail.com>
2023-11-20 22:27:05 +0100

    feat: Add default sort algh addition to URL

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,24 @@
-import createMiddleware from "next-intl/middleware";
+import createIntlMiddleware from "next-intl/middleware";
+import { NextRequest } from "next/server";
 
-export default createMiddleware({
-  locales: ["en", "pl"],
-  defaultLocale: "en",
-});
+export default async function middleware(request: NextRequest) {
+  const defaultLocale = request.headers.get("x-default-locale") || "en";
+
+  const handleI18nRouting = createIntlMiddleware({
+    locales: ["en", "pl"],
+    defaultLocale: "en",
+  });
+
+  if (!request.nextUrl.search.includes("algh")) {
+    request.nextUrl.searchParams.set("algh", "bubble");
+  }
+
+  const response = handleI18nRouting(request);
+
+  response.headers.set("x-default-locale", defaultLocale);
+
+  return response;
+}
 
 export const config = {
   matcher: ["/", "/(pl|en)/:path*"],
