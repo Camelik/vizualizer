de388f0b602647112d7afe50421ce63d7aceecf4
Kamil <menadzer.odzidzi@gmail.com>
2023-11-20 22:24:37 +0100

    style: Add necessary svgs

--- src/components/svgs/index.ts
+++ src/components/svgs/index.ts
@@ -1,2 +1,6 @@
 export { default as DarkThemeIcon } from "./darkThemeIcon.svg";
 export { default as LightThemeIcon } from "./lightThemeIcon.svg";
+export { default as PauseIcon } from "./pauseIcon.svg";
+export { default as PlayIcon } from "./playIcon.svg";
+export { default as ResetIcon } from "./resetIcon.svg";
+export { default as ArrowIcon } from "./arrowIcon.svg";
